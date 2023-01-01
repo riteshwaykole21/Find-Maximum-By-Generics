6e5a973bf1aae4ae3a28ae8791e149730c075166
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-01-01 22:25:52 +0530

    [Ritesh]add : Find Maximum Greter number among Three number

---
+++ FindMaximum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Genrics use cases");
+            integerfindmax.FindMaxValue(20, 30, 40);
+            Console.ReadLine();
+        }
+    }
+}
