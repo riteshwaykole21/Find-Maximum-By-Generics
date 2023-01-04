55a2f35fc90cd93fce894dc982c6c4ac5024021a
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-01-04 13:08:51 +0530

    [Ritesh]add : Find Maximum using Generics Method

---
+++ FindMaximum/GenericFindMax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    public class GenericFindMax<T> where T : IComparable
+    {
+        public static void FindMaxValue (T first,T second, T third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{first} is greater");
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{second} is greater");
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine($"{third} is greater ");
+            }
+            else
+                Console.WriteLine("All Number Are equal");
+        }
+    }
+}
--- FindMaximum/Program.cs
+++ FindMaximum/Program.cs
@@ -11,10 +11,27 @@ namespace FindMaximum
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Genrics use cases");
-            integerfindmax.FindMaxValue(20, 30, 40);
-            integerfindmax.FindMaxValue(20.30f, 30.45f, 65.99f);
-            integerfindmax.FindMaxValue("abc", "efg", "hij");
-            Console.ReadLine();
+            Console.WriteLine("Choose any one From Below Option");
+            Console.WriteLine("1: Find Max Num\n" + "2: FindMax Using Genrics Method");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    integerfindmax.FindMaxValue(20, 30, 40);
+                    integerfindmax.FindMaxValue(20.30f, 30.45f, 65.99f);
+                    integerfindmax.FindMaxValue("abc", "efg", "hij");
+                    break;
+                case 2:
+                    GenericFindMax<int>.FindMaxValue(11, 23, 30);
+                    GenericFindMax<float>.FindMaxValue(32.65f, 65.98f, 99.65f);
+                    GenericFindMax<string>.FindMaxValue("abc", "efg", "hij");
+                    break;
+                default:
+                    Console.WriteLine("Please Choose the Number Within Given Range");
+                    break;
+            }
+                 Console.ReadLine();
+            
         }
     }
 }
